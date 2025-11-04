c5ee394f60b108722b954b45159e7bb642554a3d
defendend <ivsacha@mail.ru>
2025-11-05 00:01:04 +0300

    Force DOM reflow after each chunk update

--- src/jsMain/kotlin/app/ChatUI.kt
+++ src/jsMain/kotlin/app/ChatUI.kt
@@ -453,6 +453,8 @@ class ChatUI {
                             fullContent += chunk
                             assistantMessage.content = fullContent
                             contentDiv?.textContent = fullContent
+                            // Force reflow to make browser render immediately
+                            contentDiv?.offsetHeight
                             console.log("Updated contentDiv, fullContent length:", fullContent.length)
                             scrollToBottom()
                         },
